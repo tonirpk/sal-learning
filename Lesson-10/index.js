7efbbe1a648785c35af025323fd310a8b6d37c31
SAL <43032308+tonirpk@users.noreply.github.com>
2022-02-14 20:37:26 +0300

    Lesson 10

---
+++ Lesson-10/index.js
@@ -0,0 +1,15 @@
+//Task one
+// let num= 123
+// console.log(num)
+
+//Task two
+// let userName='John'
+// userName='Tom'
+// console.log(userName)
+
+let num = 3
+// num++
+// num--
+// console.log(num)
+console.log(num++)
+// console.log(++num)
