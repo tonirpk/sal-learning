dbfe174631157e3406f695c242a529b1460c862f
SAL <43032308+tonirpk@users.noreply.github.com>
2022-02-14 21:58:29 +0300

    Update index.js

--- Lesson-10/index.js
+++ Lesson-10/index.js
@@ -7,9 +7,25 @@
 // userName='Tom'
 // console.log(userName)
 
-let num = 3
+//Task three
+// "" + 1 + 0 = "1" + "0" 10
+// "" - 1 + 0 = 0 -1 +0 = -1
+// true + false = 1+ 0 = 1
+// 6 / "3" = 6 / 3 = 2
+// "2" * "3" = 2 * 3 = 6
+// 4 + 5 + "px" = 9 + "px" = 9px
+// "$" + 4 + 5 = "4" + 5 = "4" + "5" = "$45"
+// "4" - 2 = 4 - 2 = 2
+// " -9 " + 5 = " -9 " + "5" = " -9 5"
+// " -9 " - 5 = -9 - 5 = -14
+// null + 1 = 1
+// undefined + 1 = NaN 
+
+// let num = 3
 // num++
 // num--
 // console.log(num)
-console.log(num++)
+// console.log(num++)
 // console.log(++num)
+
+console.log("sal" == "nigora")
